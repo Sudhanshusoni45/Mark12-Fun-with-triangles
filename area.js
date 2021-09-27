e550f712105962ea70bc8731ab2cf3fb80383162
Sudhanshusoni45 <sudhanshusoni45@gmail.com>
2021-09-27 16:26:34 +0530

    docs

---
+++ area.js
@@ -0,0 +1,17 @@
+const height = document.querySelector("#input-height");
+const base = document.querySelector("#input-base");
+const btnCalculateArea = document.querySelector("#btn-calculate-area");
+
+const divOutput = document.querySelector("#div-output");
+
+btnCalculateArea.addEventListener("click", calculateArea);
+var area = 0;
+
+function calculateArea() {
+  var heightValue = height.value;
+  var baseValue = base.value;
+  area = (heightValue * baseValue) / 2;
+  console.log("area:", area);
+
+  divOutput.innerText = area;
+}
